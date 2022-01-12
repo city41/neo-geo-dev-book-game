84938dd0e36dc54997de92ffb70ec3a32aed7349
Matt Greer <matt.e.greer@gmail.com>
2022-01-12 14:02:03 -0500

    basic ball movement

--- src/main.c
+++ src/main.c
@@ -21,15 +21,16 @@
 #define TO_SCREEN_Y(inputY) (-((inputY) + 496) - 32)
 #define TO_SCREEN_X(inputX) (inputX)
 
-struct Paddle {
+struct Entity
+{
     s16 x;
     s16 y;
+    s16 velX;
+    s16 velY;
 };
 
-struct Paddle paddle = {
-    .x = 16,
-    .y = 200
-};
+struct Entity paddle = { .x = 16, .y = 200 };
+struct Entity ball = { .x = 152, .y = 32, .velX = 1, .velY = 1 };
 
 // Load our palettes into RAM
 // MMAP_PALBANK1 is the location in main RAM where
@@ -87,9 +88,11 @@ void fix_print(u16 x, u16 y, const u8* text) {
     }
 }
 
-// of the 381 sprites available to us, we will use the first one.
+// of the 381 sprites available to us, we will use the first one for the paddle
 // we will also use sprite index 1 and 2, as our paddle is three tiles wide
-#define SPRITE_INDEX 0
+#define PADDLE_SPRITE_INDEX 0
+// and the fourth sprite for the ball
+#define BALL_SPRITE_INDEX 3
 
 // sets up the first three sprites in VRAM to be our paddle. Neo Geo
 // sprites are always a single column, so since our paddle is 3 tiles wide,
@@ -101,7 +104,7 @@ void load_paddle() {
 
     for (u8 tx = 0; tx < 3; ++tx) {
         // set the vram address register to the location for this sprite's tile map
-        *REG_VRAMADDR = ADDR_SCB1 + (SPRITE_INDEX + tx) * SCB1_SPRITE_ENTRY_SIZE;
+        *REG_VRAMADDR = ADDR_SCB1 + (PADDLE_SPRITE_INDEX + tx) * SCB1_SPRITE_ENTRY_SIZE;
         // every time we write to vram, VRAMADDR will increase by one
         *REG_VRAMMOD = 1;
 
@@ -109,15 +112,15 @@ void load_paddle() {
         // two are sticky and chain up to the first sprite
         u8 sticky = tx != 0;
 
-        // set our tile index, in this simple case, our loop variable matches
-        // the tiles in our C ROM exactly
-        *REG_VRAMRW = tx;
+        // set our tile index, in this simple case, the ball is located first
+        // in the C ROM, so we need to move past it
+        *REG_VRAMRW = tx + 4;
         // set the palette for this tile. This word also allows setting other things
         // such as whether the tile is flipped or not, but we don't need any of that today
         *REG_VRAMRW = palette << 8;
 
         // move to sprite control bank 2 to set the sprite's scaling
-        *REG_VRAMADDR = ADDR_SCB2 + SPRITE_INDEX + tx;
+        *REG_VRAMADDR = ADDR_SCB2 + PADDLE_SPRITE_INDEX + tx;
         // this allows us to jump right into SCB3 and then SCB4 after we write to SCB2
         *REG_VRAMMOD = SCB234_SIZE;
 
@@ -132,19 +135,55 @@ void load_paddle() {
     }
 }
 
+// sets up the fourth sprite in VRAM to be our paddle.
+void load_ball() {
+    // if you look in paletteDefs.c, you will see the third palette (index 2)
+    // is the correct palette for the ball
+    const u8 palette = 2;
+
+    // the ball uses auto animation, so this flag will turn it on
+    const u8 autoAnimation = 1;
+
+    // set the vram address register to the location for this sprite's tile map
+    *REG_VRAMADDR = ADDR_SCB1 + BALL_SPRITE_INDEX * SCB1_SPRITE_ENTRY_SIZE;
+    // every time we write to vram, VRAMADDR will increase by one
+    *REG_VRAMMOD = 1;
+
+    // set our tile index, in this simple case, our loop variable matches
+    // the tiles in our C ROM exactly
+    *REG_VRAMRW = 0;
+    // set the palette for this tile. This word also allows setting other things
+    // such as whether the tile is flipped or not, but we don't need any of that today
+    *REG_VRAMRW = palette << 8 | (autoAnimation << 2);
+
+    // move to sprite control bank 2 to set the sprite's scaling
+    *REG_VRAMADDR = ADDR_SCB2 + BALL_SPRITE_INDEX;
+    // this allows us to jump right into SCB3 and then SCB4 after we write to SCB2
+    *REG_VRAMMOD = SCB234_SIZE;
+
+    // set scale (horizontal and vertical)
+    *REG_VRAMRW = 0xFFF;
+
+    // now in SCB3, we set the sprite's y location and x location
+    // as well as whether it is sticky or not, and its tile height (1 in this simple case)
+    *REG_VRAMRW = (TO_SCREEN_Y(ball.y) << 7) | 1;
+    // now in SCB4, we set x position
+    *REG_VRAMRW = TO_SCREEN_X(ball.x) << 7;
+}
+
 // updates the paddle sprite's location. since we are only changing the location,
 // we only need to set a small number of values. Also since the paddle's second
 // and third sprite were set to sticky, we don't need to set their location. changing
 // the first sprite's location will cause them to automatically change too
 void move_paddle() {
     // jump to SCB3 for y, then use mod to automatically jump to SCB4 for x
-    *REG_VRAMADDR = ADDR_SCB3 + SPRITE_INDEX;
+    *REG_VRAMADDR = ADDR_SCB3 + PADDLE_SPRITE_INDEX;
     *REG_VRAMMOD = SCB234_SIZE;
 
     // set the new y and also set height to 1 again. since these two values are packed
     // into the same word, we need to make sure we set both here, otherwise the sprite
     // would disappear on screen as height would get set to zero.
-    // we don't need to set the sticky bit this time, since we are only working with the 
+    // we don't need to set the sticky bit this time, since we are only working with the
     // control sprite, we know it is not sticky so not setting it here defaults it to zero
     *REG_VRAMRW = (TO_SCREEN_Y(paddle.y) << 7) | 1;
 
@@ -152,6 +191,46 @@ void move_paddle() {
     *REG_VRAMRW = TO_SCREEN_X(paddle.x) << 7;
 }
 
+void move_ball() {
+    // jump to SCB3 for y, then use mod to automatically jump to SCB4 for x
+    *REG_VRAMADDR = ADDR_SCB3 + BALL_SPRITE_INDEX;
+    *REG_VRAMMOD = SCB234_SIZE;
+
+    // set the new y and also set height to 1 again. since these two values are packed
+    // into the same word, we need to make sure we set both here, otherwise the sprite
+    // would disappear on screen as height would get set to zero.
+    // we don't need to set the sticky bit this time, since we are only working with the
+    // control sprite, we know it is not sticky so not setting it here defaults it to zero
+    *REG_VRAMRW = (TO_SCREEN_Y(ball.y) << 7) | 1;
+
+    // set new x
+    *REG_VRAMRW = TO_SCREEN_X(ball.x) << 7;
+}
+
+void ball_logic() {
+    ball.x += ball.velX;
+    ball.y += ball.velY;
+
+    if (ball.x < 0) {
+        ball.x = 0;
+        ball.velX *= -1;
+    }
+
+    if (ball.x > 304) {
+        ball.x = 304;
+        ball.velX *= -1;
+    }
+
+    if (ball.y < 0) {
+        ball.y = 0;
+        ball.velY *= -1;
+    }
+
+    if (ball.y > 208) {
+        ball.y = 208;
+        ball.velY *= -1;
+    }
+}
 
 // this is set to volatile because it will get changed in our
 // vblank callback. that callback is invoked by the bios, so gcc
@@ -159,46 +238,53 @@ void move_paddle() {
 // keyword is our way of telling gcc "be aware something outside
 // our own code can change this value, so don't assume it is whatever
 // we last set it to"
-volatile u8 vblank=0;
+volatile u8 vblank = 0;
 
 // ngdevkit calls this function whenever vblank happens. It must
 // have this exact name, as that is how ngdevkit will find it
 void rom_callback_VBlank() {
-    vblank=1;
+    vblank = 1;
 }
 
 // we want to wait for vblank before changing vram, so we
 // just loop and continually check if the vblank callback has happened
 void wait_vblank() {
-    while (!vblank);
-    vblank=0;
+    while (!vblank)
+        ;
+    vblank = 0;
 }
 
 // ngdevkit will call main() once it is ready to run your game,
 int main() {
+    // set the auto animation speed
+    *REG_LSPCMODE = (6 << 8);
+
     init_palettes();
     fix_clear();
-    
+
     // these two calls are changing VRAM and we have no idea
     // what state the screen is currently in. so these two
     // calls could cause a bit of onscreen garbage. but for
     // this simple demo, it's fine
     fix_print(3, 4, "Use left and right to move paddle");
     load_paddle();
+    load_ball();
 
     for (;;) {
         //  bios_p1current has the current state of player 1's joystick and
         // A, B, C and D buttons. If the CNT_LEFT bit is high, that means left
         // is currently being pressed
         if (bios_p1current & CNT_LEFT) {
-            paddle.x -= 1;
+            paddle.x -= 3;
         }
 
         // and same thing for right
         if (bios_p1current & CNT_RIGHT) {
-            paddle.x += 1;
+            paddle.x += 3;
         }
-        
+
+        ball_logic();
+
         // we don't want to change the paddle's sprite location
         // until vblank has occured, that way the screen always
         // appears consistent to the player
@@ -207,6 +293,7 @@ int main() {
         // to update VRAM. thankfully we aren't doing much at all, so we
         // will be just fine
         move_paddle();
+        move_ball();
     }
 
     // we never actually get to this return, but we will
