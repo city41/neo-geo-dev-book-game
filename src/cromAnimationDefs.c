46ec47336bcef0cd8a894ea98bd9f36ffcaf38d4
Matt Greer <matt.e.greer@gmail.com>
2023-10-07 12:10:05 -0400

    sromcrom: ejs to hbr

--- src/cromAnimationDefs.c
+++ src/cromAnimationDefs.c
@@ -5,25 +5,25 @@
 const struct FrameDef frameDefs_ball_spin[4] = {
     {
         .tileIndex = 3,
-        .paletteIndex = 2
+        .paletteIndex = 2,
     },
     {
         .tileIndex = 4,
-        .paletteIndex = 2
+        .paletteIndex = 2,
     },
     {
         .tileIndex = 5,
-        .paletteIndex = 2
+        .paletteIndex = 2,
     },
     {
         .tileIndex = 6,
-        .paletteIndex = 2
+        .paletteIndex = 2,
     }
 };
 
 const struct AnimationDef animationDef_ball_spin = {
     .frames = frameDefs_ball_spin,
-    .frameCount = 4,
+    .frameCount = 4, 
     .frameDuration = 10
 };
 
--- src/cromAnimationDefs.h
+++ src/cromAnimationDefs.h
@@ -1,11 +1,9 @@
 #pragma once
 #include "animationDef.h"
 
-
 // start ball
 
 const struct AnimationDef animationDef_ball_spin;
-
 
 // end ball
 
